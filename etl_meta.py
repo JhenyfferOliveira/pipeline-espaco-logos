65fbd4cc5956fc31efd5bfe37bf7ba65f0600d09
JhenyfferOliveira <jhenyffer.trad@gmail.com>
2025-08-09 01:59:19 -0300

    Atualizando repo virtual

--- etl_meta.py
+++ etl_meta.py
@@ -3,11 +3,13 @@ import pandas as pd
 from datetime import datetime
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+import json
+import os
 
-TOKEN_DE_ACESSO = "EAAIr3fzAZBE4BPMgZCU1RaojNCbF2qJ0nI3nu5znvXquaqEILEHxKMWlFPqJ7TG8JC9vZBotv90mzZCxC5tn1dPqPYTZCIrBEfRsNcObbhSgKobkgQ9HWKvtLsZBqj7gmB1bxNecolywZCNewPi1rEnWUI1kYPZA2qvTgJLweKqbNLc5edWizjnO3JZCR55uRLJ9ixln0Ccg75P0ZB0ByTycPMB0xy"
-ID_USUARIO_INSTAGRAM = "17841403166668009"
-GOOGLE_CREDENTIALS_JSON = "credenciais.json"
-NOME_PLANILHA = "ong_info_raw"
+TOKEN_DE_ACESSO = os.environ.get("TOKEN_DE_ACESSO")
+ID_USUARIO_INSTAGRAM = os.environ.get("ID_USUARIO_INSTAGRAM")
+GOOGLE_CREDENTIALS_JSON = os.getenv("GOOGLE_CREDENTIALS_JSON")
+NOME_PLANILHA = os.getenv("ong_info_raw")
 
 def buscar_midias_do_usuario():
     url = f"https://graph.facebook.com/v19.0/{ID_USUARIO_INSTAGRAM}/media"
